bad09a5d1b778e7fe2f44f0078874ee66c2fc82a
Tinghao <fengt@appstate.edu>
2023-04-20 20:23:36 -0400

    update

--- src/Components/Line.js
+++ src/Components/Line.js
@@ -1,19 +1,22 @@
 import * as d3 from 'd3';
 
 function Line({ data, scale, label }) {
-    const path = d3.line()
-        .x((d, i) => scale.x(i))
-        .y(d => scale.y(d))
-        (data);
     /*
-        Define the line using d3.line() for the single line.
+        *********************************************************************
+        * Define the line using d3.line() for the single line.              *
+        * Applying x scale and y scale completes the following code         *
+        *   const path = d3.line()                                          *
+        *       .x(...)                                                     *
+        *       .y(...)                                                     *
+        *       (data);                                                     *
+        * *******************************************************************
     */
     return (
         <path />
         /*
             *****************************************************************************************
-            * Define the line with the following attributes                      *
-            *   d = {...} - the path, which has been defined earlier                                *
+            * Define the line with the following attributes                                         *
+            *   d = {path} - the path, which has been defined earlier                                *
             *   stroke = {...} - the color, which should be defined by the color scale and label    *
             *   fill = 'none' -  should explicitly specify no fill for the line                     *
             *   strokeWidth = '2' - you can customize the width of this line                        *
